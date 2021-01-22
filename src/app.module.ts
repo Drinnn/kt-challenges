92a9e670f046a4ef1e257c00c5314b81763c846a
Drinnn <pedro_lontro@hotmail.com>
2021-01-22 14:05:23 -0300

    feat: add challenges module skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChallengesModule } from './challenges/challenges.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ChallengesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
